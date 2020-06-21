6495fd138b6539c65948d3fb9e6e6d881beeb8a2
LercheKa <64099274+LercheKa@users.noreply.github.com>
2020-06-21 16:39:32 +0200

    Aufgabe8?

--- Aufgabe8/drumpad.ts
+++ Aufgabe8/drumpad.ts
@@ -1,45 +1,68 @@
-// Aufgabe 7.1
-
-var sounds: HTMLAudioElement [] = [];
-        sounds.push (new Audio ("assets/A.mp3"));
-        sounds.push (new Audio ("assets/C.mp3"));
-        sounds.push (new Audio ("assets/F.mp3"));
-        sounds.push (new Audio ("assets/G.mp3"));
-        sounds.push (new Audio ("assets/hihat.mp3"));
-        sounds.push (new Audio ("assets/kick.mp3"));
-        sounds.push (new Audio ("assets/laugh-1.mp3"));
-        sounds.push (new Audio ("assets/laugh-2.mp3"));
-        sounds.push (new Audio ("assets/snare.mp3"));
+var sounds1 = ["A.mp3", "C.mp3", "F.mp3", "G.mp3", "hihat.mp3", "kick.mp3", "laugh-1.mp3", "laugh-2.mp3", "snare.mp3"];
 
 function playSample(samplenumber: number): void {
-    sounds[samplenumber].play();
+    var allsounds: HTMLAudioElement = new Audio(sounds1[samplenumber]);
+    allsounds.play();
+    if (recording == true) {
+        beat.push(sounds1[samplenumber]);
+    }
 }
 
-window.addEventListener("load", function() {
-    document.querySelector("#b1").addEventListener("click", function() { playSample(0); });
-    document.querySelector("#b2").addEventListener("click", function() { playSample(1); });
-    document.querySelector("#b3").addEventListener("click", function() { playSample(2); });
-    document.querySelector("#b4").addEventListener("click", function() { playSample(3); });
-    document.querySelector("#b5").addEventListener("click", function() { playSample(4); });
-    document.querySelector("#b6").addEventListener("click", function() { playSample(5); });
-    document.querySelector("#b7").addEventListener("click", function() { playSample(6); });
-    document.querySelector("#b8").addEventListener("click", function() { playSample(7); });
-    document.querySelector("#b9").addEventListener("click", function() { playSample(8); });
+document.querySelector(".button1").addEventListener("click", function () { playSample(0); });
+document.querySelector(".button2").addEventListener("click", function () { playSample(1); });
+document.querySelector(".button3").addEventListener("click", function () { playSample(2); });
+document.querySelector(".button4").addEventListener("click", function () { playSample(3); });
+document.querySelector(".button5").addEventListener("click", function () { playSample(4); });
+document.querySelector(".button6").addEventListener("click", function () { playSample(5); });
+document.querySelector(".button7").addEventListener("click", function () { playSample(6); });
+document.querySelector(".button8").addEventListener("click", function () { playSample(7); });
+document.querySelector(".button9").addEventListener("click", function () { playSample(8); });
 
+//Loop
+var beat: string[] = ["hihat.mp3", "kick.mp3", "laugh-2.mp3"];
+var meinInterval: number;
+function defbeat(): void {
+    var meineZaehlervariable: number = 0;
+    console.log(meineZaehlervariable);
+    console.log(beat[meineZaehlervariable]);
 
-// Aufgabe 7.2
-
-function drumpadbeat() {
-    setInterval(function() {
-        playSample(5); 
-        playSample(7); 
-        playSample(8)
-    }, 500);
+    meinInterval = setInterval(function () {
+        var playAudio: HTMLAudioElement = new Audio(beat[meineZaehlervariable]);
+        playAudio.play();
+        meineZaehlervariable++;
+        if (meineZaehlervariable >= 3) {
+            meineZaehlervariable = 0;
+        }
+    },                         500);
 }
 
-
-document.querySelector("#bplay").addEventListener("click", drumpadbeat);
-
- 
-
+//Play und Pause
+var playbutton = document.querySelector("#play");
+playbutton.addEventListener("click", function () {
+    if (playbutton.getAttribute("class") == "fas fa-play") {
+        playbutton.setAttribute("class", "fas fa-pause");
+        defbeat();
+    }
+    else {
+        playbutton.setAttribute("class", "fas fa-play");
+        clearInterval(meinInterval);
+    }
 });
+
+//Clear
+document.querySelector("#clear").addEventListener("click", function () {clearbeat(); });
+function clearbeat(): void {
+    beat.length = 0;
+}
+//Inspiriert durch Anna Kaufmann
+
+//Record
+document.querySelector("#record").addEventListener("click", function () {recordbeat(); });
+var recording: boolean = false;
+function recordbeat(): void {
+    if (recording == false) {
+        recording = true;
+    }
+    else {
+        recording = false;
+    }
