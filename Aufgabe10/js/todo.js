b9c673f66ecb45dd6047e1009d72412c81dad442
LercheKa <64099274+LercheKa@users.noreply.github.com>
2020-07-11 22:42:58 +0200

    Aufgabe 11

--- Aufgabe10/js/todo.js
+++ Aufgabe10/js/todo.js
@@ -67,4 +67,33 @@ function deleteTodo(index) {
     toDoList.splice(index, 1);
     drawListToDOM();
 }
+window.addEventListener("load", function () {
+    const artyom = new Artyom();
+    artyom.addCommands({
+        indexes: ["erstelle Aufgabe *"],
+        smart: true,
+        action: function (i, wildcard) {
+            console.log("Neue Aufgabe wird erstellt: " + wildcard);
+            toDoList.unshift({ text: wildcard,
+                checked: false
+            });
+            drawListToDOM();
+        }
+    });
+    function startContinuousArtyom() {
+        artyom.fatality();
+        setTimeout(function () {
+            artyom.initialize({
+                lang: "de-DE",
+                continuous: true,
+                listen: true,
+                interimResults: true,
+                debug: true
+            }).then(function () {
+                console.log("Ready!");
+            });
+        }, 250);
+    }
+    startContinuousArtyom();
+});
 //# sourceMappingURL=todo.js.map
